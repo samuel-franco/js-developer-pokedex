3b5ed64171a661efc311ce81902f91cb15ce7945
Samuel Silva <samuelfranko66@gmail.com>
2022-11-16 23:55:41 -0300

    adição de modificação

---
+++ JavaScript/scriptmain.js
@@ -0,0 +1,19 @@
+
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
+
+fetch(url)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (jsonBody) {
+        console.log(jsonBody)
+    })
+    .catch(function (error) {
+        console.error(error)
+    })
+    .finally(function () {
+        console.log('Requisição concluída!')
+    })
+   
